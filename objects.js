5b6dba1108897b08c1a51cbadcdf0def00442796
Miyuki Abe <miyuki0809@gmail.com>
2018-04-20 02:02:18 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,6 +9,6 @@ function updatePlaylist (playlist, Name, Title){
   playlist[Name] = 'Title'
 }
 
-function removeFromPlaylist(obj, key) {
+function removeFromPlaylist (playlist, artistName) {
   delete obj.key;
 }
