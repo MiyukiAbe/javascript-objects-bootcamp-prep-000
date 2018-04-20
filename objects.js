20da4ccfb690602ec996f740e2a9b10e5606d2c7
Miyuki Abe <miyuki0809@gmail.com>
2018-04-20 01:33:51 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist = {
 };
 
 function updatePlaylist (playlist, artistName, songTitle) {
-  playlist.ArtistName = "songTitle";
+  playlist['ArtistName'] = 'songTitle';
   return playlist
   
 }
